e6b7dd60a9efc18e0e79bbedc55ce73ed53c6a72
Ankit Prasad <ankitprasad.id@gmail.com>
2020-05-10 00:23:46 +0530

    Update Main.java

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -3,7 +3,7 @@ import calculator.*;
 import java.util.*;
 public class Main
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws Exception
     {
         int flag=0,ch;
         double num1,num2;
